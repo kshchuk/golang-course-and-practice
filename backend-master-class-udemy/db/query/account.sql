289efba560895df5967d846fd8b7f158447bf3ef
kshchuk <kshchuk@gmail.com>
2023-07-18 23:31:40 +0300

    Update account.sql
    Added two queries

--- backend-master-class-udemy/db/query/account.sql
+++ backend-master-class-udemy/db/query/account.sql
@@ -12,6 +12,11 @@ RETURNING *;
 SELECT * FROM public.account 
 WHERE account_id = $1 LIMIT 1;
 
+-- name: GetAccountForUpdate :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1
+FOR NO KEY UPDATE;
+
 -- name: ListAсcounts :many
 SELECT * FROM public.account
 LIMIT $1
@@ -24,6 +29,12 @@ SET
   balance = $3,
   currency = $4
 WHERE account_id = $1
+RETURNING *;
+
+-- name: AddAccountBalance :one
+UPDATE accounts
+SET balance = balance + sqlc.arg(amount)
+WHERE id = sqlc.arg(id)
 RETURNING *;
 
 -- name: DeleteAccount :exec
