84d9626eecced32d955fde96657f4d3de6c82f22
kshchuk <kshchuk@gmail.com>
2023-08-09 16:41:51 +0300

    Added viper

--- backend-master-class-udemy/db/sqlc/main_test.go
+++ backend-master-class-udemy/db/sqlc/main_test.go
@@ -6,20 +6,21 @@ import (
 	"os"
 	"testing"
 
-	_ "github.com/lib/pq"
-)
+	"tutorial.sqlc.dev/app/utils"
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5431/simple_bank?sslmode=disable"
+	_ "github.com/lib/pq"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := utils.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config: " + err.Error())
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: " + err.Error())
 	}
--- backend-master-class-udemy/main.go
+++ backend-master-class-udemy/main.go
@@ -6,17 +6,18 @@ import (
 
 	"tutorial.sqlc.dev/app/api"
 	db "tutorial.sqlc.dev/app/db/sqlc"
+	"tutorial.sqlc.dev/app/utils"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5431/simple_bank?sslmode=disable"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := utils.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config: " + err.Error())
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: " + err.Error())
 	}
@@ -24,7 +25,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(":8080")
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server: " + err.Error())
 	}
